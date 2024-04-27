f5bc07b3f1ce9e310e0c23c6f7dcea87b36611df
kulasisin <107702039@mail2.nccu.tw>
2024-04-27 21:42:49 +0800

    socket function new-texture light-on /light-off

---
+++ main.js
@@ -0,0 +1,31 @@
+// 創建Socket.io客戶端連接
+import { io } from "https://cdn.socket.io/4.7.5/socket.io.esm.min.js";
+const socket = io("http://localhost:8080", {
+  withCredentials: false,
+});
+
+// 當使用者點擊開燈按鈕時觸發
+document
+  .getElementById("light-on-button")
+  .addEventListener("click", function () {
+    const requestId = "662cadae8aaeb0ee47a68ece"; // 假設這是請求的ID
+    socket.emit("light-on", { _id: requestId });
+    console.log("發送開燈訊號至伺服器");
+  });
+
+// 監聽後端發送的 light-on 訊號
+socket.on("light-on", function (data) {
+    console.log("伺服器收到開燈訊號:", data);
+  });
+
+  document
+  .getElementById("light-off-button")
+  .addEventListener("click", function () {
+    const requestId = "662cadae8aaeb0ee47a68ece"; // 假設這是請求的ID
+    socket.emit("light-off", { _id: requestId });
+    console.log("發送關燈訊號至伺服器");
+  });
+
+  socket.on("light-off", function (data) {
+    console.log("伺服器收到關燈訊號:", data);
+  });

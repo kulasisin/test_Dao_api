6e0cd22bdc526e79d84a489e22c230bc74c92bda
kulasisin <107702039@mail2.nccu.tw>
2024-04-29 08:54:24 +0800

    change light id input

--- main.js
+++ main.js
@@ -8,24 +8,24 @@ const socket = io("https://daotaiwanapi.onrender.com", {
 document
   .getElementById("light-on-button")
   .addEventListener("click", function () {
-    const requestId = "662a5cdde3dacb847ced645d"; // 假設這是請求的ID
+    const requestId = document.getElementById("light-id").value; // 取得 input id 的值
     socket.emit("light-on", { _id: requestId });
     console.log("發送開燈訊號至伺服器");
   });
 
 // 監聽後端發送的 light-on 訊號
 socket.on("light-on", function (data) {
-    console.log("伺服器收到開燈訊號:", data);
-  });
+  console.log("伺服器收到開燈訊號:", data);
+});
 
-  document
+document
   .getElementById("light-off-button")
   .addEventListener("click", function () {
-    const requestId = "662a5cdde3dacb847ced645d"; // 假設這是請求的ID
+    const requestId = document.getElementById("light-id").value; // 取得 input id 的值
     socket.emit("light-off", { _id: requestId });
     console.log("發送關燈訊號至伺服器");
   });
 
-  socket.on("light-off", function (data) {
-    console.log("伺服器收到關燈訊號:", data);
-  });
+socket.on("light-off", function (data) {
+  console.log("伺服器收到關燈訊號:", data);
+});

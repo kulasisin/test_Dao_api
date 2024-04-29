14391a90a831c15ca5e61227e3a4cacdd96a21ee
kulasisin <107702039@mail2.nccu.tw>
2024-04-29 16:02:51 +0800

    socket light add category

--- main.js
+++ main.js
@@ -9,8 +9,7 @@ document
   .getElementById("light-on-button")
   .addEventListener("click", function () {
     const requestId = document.getElementById("light-id").value; // 取得 input id 的值
-    socket.emit("light-on", { _id: requestId });
-    console.log("發送開燈訊號至伺服器");
+    fetchTextureById(requestId, "light-on");
   });
 
 // 監聽後端發送的 light-on 訊號
@@ -22,10 +21,21 @@ document
   .getElementById("light-off-button")
   .addEventListener("click", function () {
     const requestId = document.getElementById("light-id").value; // 取得 input id 的值
-    socket.emit("light-off", { _id: requestId });
+    fetchTextureById(requestId, "light-off");
     console.log("發送關燈訊號至伺服器");
   });
 
 socket.on("light-off", function (data) {
   console.log("伺服器收到關燈訊號:", data);
 });
+
+function fetchTextureById(id, event) {
+  fetch(`https://daotaiwanapi.onrender.com/image/${id}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(`獲取${id}貼圖資料`, data.data.category);
+      const category = data.data.category;
+      socket.emit(`${event}`, { _id: id, category: category });
+    })
+    .catch((error) => console.error("貼圖資料獲取失敗:", error));
+}

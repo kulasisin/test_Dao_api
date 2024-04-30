bb16c2e3bd36a2217ed3853c072a3a5bd7080eea
kulasisin <107702039@mail2.nccu.tw>
2024-05-01 01:41:20 +0800

    fix socket add test buttons

--- main.js
+++ main.js
@@ -1,9 +1,115 @@
+ // 初始加载图片列表
+ fetchImages();
+ fetchProcessedImages();
+
+ // 獲取圖片列表函數
+ function fetchImages() {
+   const xhr = new XMLHttpRequest();
+   // xhr.open("GET", "https://daotaiwanapi.onrender.com/images");
+
+   xhr.open("GET", "https://daotaiwanapi.onrender.com/images");
+   xhr.onload = function () {
+     if (xhr.status === 200) {
+       const images = JSON.parse(xhr.responseText).data;
+       const imageList = document.getElementById("imageList");
+       imageList.innerHTML = ""; // 清空先前的列表
+
+       const ul = document.createElement("ul"); // 創建 ul 元素
+
+       images.reverse().forEach((image) => {
+         const li = document.createElement("li"); // 創建 li 元素
+         const img = document.createElement("img");
+         img.src = image.url; // 使用 URL
+         img.alt = image.filename;
+         // 创建开灯按钮
+         const lightOnButton = document.createElement("button");
+           lightOnButton.textContent = "開燈測試";
+           lightOnButton.addEventListener("click", () => fetchTextureById(image._id, "light-on"));
+
+           // 创建关灯按钮
+           const lightOffButton = document.createElement("button");
+           lightOffButton.textContent = "關燈測試";
+           lightOffButton.addEventListener("click", () =>  fetchTextureById(image._id, "light-off"));
+
+           // 创建显示 _id 的输入框
+           const idInput = document.createElement("input");
+           idInput.type = "text";
+           idInput.value = image._id;
+           idInput.readOnly = true;
+         li.appendChild(img); // 把 img 放到 li 中
+         li.appendChild(lightOnButton);
+         li.appendChild(lightOffButton);
+         li.appendChild(idInput);
+         ul.appendChild(li); // 把 li 放到 ul 中
+       });
+       imageList.appendChild(ul); // 把 ul 放到 imageList 中
+     } else {
+       console.error("獲取圖片列表失敗");
+     }
+   };
+   xhr.send();
+ }
+ // 新增渲染成果圖片列表
+ function createProcessedList() {
+   const list = document.createElement("ul");
+   list.id = "processedImageList";
+   document.body.appendChild(list);
+   return list;
+ }
+ //獲取渲染成果圖
+ function fetchProcessedImages() {
+   fetch("https://daotaiwanapi.onrender.com/results")
+     .then((response) => response.json())
+     .then((data) => {
+       const processedImageList =
+         document.getElementById("processedImageList");
+       processedImageList.innerHTML = "";
+       const ul = document.createElement("ul");
+
+       data.data.reverse().forEach((image) => {
+         const li = document.createElement("li");
+         const img = document.createElement("img");
+         img.src = image.url;
+         img.alt = image.filename;
+         li.appendChild(img);
+         ul.appendChild(li);
+       });
+       processedImageList.appendChild(ul);
+     })
+     .catch((error) =>
+       console.error("Failed to fetch processed images:", error)
+     );
+ }
+ function uploadImage() {
+   const formData = new FormData(document.getElementById("uploadForm"));
+
+   console.log(formData);
+   fetch("https://daotaiwanapi.onrender.com/upload", {
+     method: "POST",
+     body: formData,
+   })
+     .then((response) => response.json())
+     .then((data) => {
+       console.log(data);
+       alert(JSON.stringify(data.message));
+       fetchImages(); // 重新加载图片列表
+     })
+     .catch((error) => {
+       console.error("Error:", error);
+       alert("Failed to upload image");
+     });
+ }
+
+
 // 創建Socket.io客戶端連接
 import { io } from "https://cdn.socket.io/4.7.5/socket.io.esm.min.js";
 const socket = io("https://daotaiwanapi.onrender.com", {
   withCredentials: false,
 });
-
+// 監聽後端發送的 message 訊號
+socket.on("message", function (data) {
+  console.log("伺服器傳送訊息:", data);
+});
 // 當使用者點擊開燈按鈕時觸發
 document
   .getElementById("light-on-button")
@@ -39,3 +145,4 @@ function fetchTextureById(id, event) {
     })
     .catch((error) => console.error("貼圖資料獲取失敗:", error));
 }
+
